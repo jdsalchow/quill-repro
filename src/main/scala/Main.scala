0cb73806851c5ffeb26cb6e775238441b163a6b4
Jan-David Salchow <jdsalchow@gmail.com>
2022-05-21 19:17:04 +0200

    Add compiler messages and output as comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -22,5 +22,13 @@ case class Bar(id: Int, fooId: Int, attribute: String)
     foo <- query[Foo]
     bar <- query[Bar].leftJoin(bar => bar.fooId == foo.id)
   } yield (foo, bar))
+  // [info] 21 |  val result = run(for {
+  // [info]    |               ^
+  // [info]    |Quill Query: SELECT foo.id, foo.attribute, bar.id, bar.foo_id AS fooId, bar.attribute FROM foo foo LEFT JOIN bar bar ON bar.foo_id = foo.id
+  // [info] 22 |    foo <- query[Foo]
+  // [info] 23 |    bar <- query[Bar].leftJoin(bar => bar.fooId == foo.id)
+  // [info] 24 |  } yield (foo, bar))
+  // [WARNING] Arity of product column "_2" type Option[Bar] was 0. This is not valid.
 
   println(result)
+  // gives: List((Foo(1,a),Some(Bar(1,1,c))), (Foo(1,a),Some(Bar(2,1,d))), (Foo(2,b),Some(Bar(0,0,null))))
